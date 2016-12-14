4445fe2bad7dc441d924e80033fff31b0d4db112
AndrewYW <andrewwang39@gmail.com>
2016-12-13 23:32:41 -0500

    redo initialization

---
+++ app/src/main/java/me/andrewyw/android79/Chess.java
@@ -0,0 +1,13 @@
+package me.andrewyw.android79;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chess);
+    }
+}
